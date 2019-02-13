0f07b5647f89ea6200dd0d1bfdde14c2366993a0
Lay <1778038380@qq.com>
2019-02-13 10:07:57 +0800

    多切面链式执行

--- smartframework/src/main/java/com/lay/smartframework/proxy/ProxyManager.java
+++ smartframework/src/main/java/com/lay/smartframework/proxy/ProxyManager.java
@@ -15,6 +15,8 @@ import java.util.List;
  */
 public class ProxyManager {
     public static<T> T createProxy(final Class<?> targetClass, final List<Proxy> proxyList){
-        return (T) Enhancer.create(targetClass, (MethodInterceptor) (o, method, objects, methodProxy) -> new ProxyChain(targetClass,o,method,methodProxy,objects,proxyList).doProxyChain());
+        return (T) Enhancer.create(targetClass, (MethodInterceptor) (o, method, objects, methodProxy) -> {
+            return new ProxyChain(targetClass, o, method, methodProxy, objects, proxyList).doProxyChain();
+        });
     }
 }
