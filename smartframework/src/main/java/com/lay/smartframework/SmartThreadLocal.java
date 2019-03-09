0958748a8200ae3920e55c1fa0446514451318ee
lyn <237263339@qq.com>
2019-03-09 23:55:39 +0800

    自己实现ThreadLocal，线程同步

---
+++ smartframework/src/main/java/com/lay/smartframework/SmartThreadLocal.java
@@ -0,0 +1,35 @@
+package com.lay.smartframework;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SmartThreadLocal<T> {
+    private Map<Thread,T> container= Collections.synchronizedMap(new HashMap<Thread, T>());
+
+    public void set(T value){
+        container.put(Thread.currentThread(),value);
+    }
+
+    public T get(){
+        Thread thread = Thread.currentThread();
+        T value=container.get(thread);
+        if(value==null&&!container.containsKey(thread)){
+            value=initialValue();
+            container.put(thread,value);
+
+        }
+        return value;
+
+    }
+
+    public void remove(){
+        container.remove(Thread.currentThread());
+    }
+
+    private T initialValue() {
+        return  null;
+    }
+
+
+}
